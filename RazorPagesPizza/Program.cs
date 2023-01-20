d710e0a49d70a380d0615a52b6aebbe2ae11fe76
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-01-20 20:25:58 +0100

    Adding a QR code service and registering it in IoC

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using QRCoder;
 using RazorPagesPizza.Areas.Identity.Data;
 using RazorPagesPizza.Services;
 
@@ -16,6 +17,7 @@ builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.Sign
 
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.AddSingleton(new QRCodeService(new QRCodeGenerator()));
 
 var app = builder.Build();
 
